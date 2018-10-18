a23cf9354287a9afb8a88b1612ef6be6ebbf031f
Junghwan Song <jhsong@mmlab.snu.ac.kr>
2018-10-18 14:05:26 +0900

    Add ns-3 essentials

--- configure.sh
+++ configure.sh
@@ -1,6 +1,14 @@
 #environment: Ubuntu 16.04LTS, 18.04LTS
 
+#install ns-3 essentials
+#default
+sudo apt-get install -y gcc g++ python python-dev python-setuptools
+#visualization
+sudo apt-get install -y qt5-default python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython
+#debug
+sudo apt-get install -y gdb valgrind 
 
+#configure ofswitch13 module
 sudo apt-get install -y build-essential gcc g++ python git mercurial unzip cmake
 sudo apt-get install -y libpcap-dev libxerces-c-dev libpcre3-dev flex bison
 sudo apt-get install -y pkg-config autoconf libtool libboost-dev
